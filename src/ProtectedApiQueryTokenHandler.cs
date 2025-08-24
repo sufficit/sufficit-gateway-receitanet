dd3f6938c22b55c865ebea2008827314d98dbaa9
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-08-23 21:09:28 -0300

    Remover referências desnecessárias e atualizar versões de pacotes no projeto

--- src/ProtectedApiQueryTokenHandler.cs
+++ src/ProtectedApiQueryTokenHandler.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
